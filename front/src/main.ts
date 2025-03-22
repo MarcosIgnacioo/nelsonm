05907a5e05f9ec4eee3aa52ff67ff81bf9fba3a1
MarcosIgnacioo <magma_ruby145@hotmail.com>
2025-03-22 05:15:55 +0000

    asdf

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,13 +1,18 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<div>
+  <img src="https://media2.giphy.com/media/YTtqB2j5EN7IA/200w.gif?cid=6c09b952avx9x0b6g87aitmsno4dk223nzhkjc4wj6naz1ye&ep=v1_gifs_search&rid=200w.gif&ct=g"/>
+        <label for="ip">ip del server d minecraft hipixel</label>
+        <input type="text" id="ip" value="">
+  </div>
   <form>
         <label for="image_field">sube la imagen amigaaaa</label>
         <input type="file" name="image_field" value="">
         <button type="submit">PICALE</button>
     </form>
   <div id="pics">
-    <h1>Fotos</h1>
+    <span>Fotos</span>
   </div>
 `
 
@@ -15,8 +20,9 @@ const form = document.querySelector("form")
 
 form?.addEventListener('submit', async (e: Event) => {
   e.preventDefault();
-  //const server = "http://25.0.119.160:4076/v1/image"
-  const server = "http://localhost:4076/v1/image"
+  const ip = document.querySelector("#ip") as HTMLInputElement
+  const server = `http://${ip?.value}:4076/v1/image`
+  //const server = "http://localhost:4076/v1/image"
   const image_input = document.getElementsByName('image_field')[0] as HTMLInputElement;
   const files = image_input.files
   if (files == null || !files) return alert("bro upload af ile")
@@ -30,7 +36,9 @@ form?.addEventListener('submit', async (e: Event) => {
     .then(async (response) => {
       const json = await response.json()
       const pics = document.getElementById('pics')
-      pics?.append(`${json.file_name}`)
+      const span: HTMLSpanElement = document.createElement('span');
+      span.innerText = json.file_name
+      pics?.appendChild(span)
     })
     .catch(err => alert(err));
 })
